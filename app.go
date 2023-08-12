644f2f64f2912423195eea1b10f082a48641f2ea
odeladetunji <odeladetunji@gmail.com>
2023-08-12 17:15:29 +0100

    Committing

---
+++ app.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+
+	router := gin.Default();
+	router.Static("/assets", "./assets");
+	router.LoadHTMLFiles("./templates/app.html");
+
+	router.GET("/", func(c *gin.Context){
+		c.HTML(http.StatusOK, "app.html", gin.H{
+			"message": "Hypersonic Inc",
+		})
+	})
+
+	if err := router.Run(":8086"); err != nil {
+		fmt.Println("Go server is running on port 8086")
+	}
+
+}
